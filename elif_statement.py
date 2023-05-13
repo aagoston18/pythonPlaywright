9948495d7e7d0117529878bbc6e59afac66082f9
Alpar Agoston <alparagoston@gmail.com>
2023-05-13 13:28:52 +0300

    elif_statement exercises and notes

---
+++ elif_statement.py
@@ -0,0 +1,19 @@
+a = 3
+
+if a < 10:
+    print(f'"3 is less than 10"')
+elif a > 10:
+    print(f'"3 is greater than 10"')
+elif a == 3:
+    print(f'"3 is equal to 3"')
+else:
+    print(f'"error"')
+
+print(f'"end of the progpram"')
+
+# code always checks for 'if' statements
+# if the 'if' statement is FALSE, it will look for other 'if' / 'elif' statements
+# if all 'if' / 'elif' statements are FALSE, code will switch to 'else'
+
+
+#python uses 4 spaces or TAB indentation
