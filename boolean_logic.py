8283cc469a226c2282edeae8986ecbd00a4b9387
Alpar Agoston <alparagoston@gmail.com>
2023-05-13 13:47:29 +0300

    Logic cheat sheet and exercises

---
+++ boolean_logic.py
@@ -0,0 +1,37 @@
+# Cheat Sheet
+# TRUE and TRUE => TRUE
+# TRUE and FALSE => TRUE
+# FALSE and TRUE => FALSE
+# FALSE and FALSE => FALSE
+
+#exercise
+a = 3
+b = 4
+c = 5
+# print(f'{a == 3} and {b == 4} and {c ==5}')
+# print(f' {a == 33} or {b == 4}')
+
+print (a ==33 and b ==44)
+print (a ==33 or b == 4)
+print (a == 3 or c == 1)
+print (a == 11 or c == 55 and b == 44)
+
+print((1 + 2) ** 3 * 5 - 2)
+
+print (a == 3 or (b == 44 and c == 55))
+
+
+# AND operator
+# print(f'"true and true is :: " {True and True}') #True
+# print(f'"false and false is :: "{False and False}') #False
+# print(f'"true and false is :: " {True and False}') #False
+# print(f'"false and true is :: " {False and True}') #False
+
+
+# OR operator
+# print(f'true or false is ::  {True or False} ') #TRUE
+# print(f'false or true is ::  {False or True}') #TRUE
+# print(f'true or true is ::  {True or True}') #TRUE
+# print(f'false or false is ::  {False or False}') #FALSE
+
+
